6c7dcbd7f33ba69b6d5b598d424c9616e35174a1
Witherking25 <witherking@withertech.com>
2022-11-17 10:15:03 -0500

    Fixed minified react error regarding my cutsom patching solution by not using my custom patching solution

--- src/AppPatch.tsx
+++ src/AppPatch.tsx
@@ -1,4 +1,4 @@
-import {ServerAPI, ServerResponse} from "decky-frontend-lib";
+import {afterPatch, ServerAPI, ServerResponse} from "decky-frontend-lib";
 import {ReactElement} from "react";
 import {PlayTimes} from "./Interfaces";
 import {Patching} from "./Patching";
@@ -6,8 +6,10 @@ import {Patching} from "./Patching";
 export const patchAppPage = (serverAPI: ServerAPI) => {
 	return serverAPI.routerHook.addPatch("/library/app/:appid", (props: { path: string, children: ReactElement}) =>
 	{
-		Patching.patch(props.children, {})
-				.afterPatchBasic(ret => ret.props, "renderFunc", (_, ret, vars) =>
+		afterPatch(
+				props.children.props,
+				"renderFunc",
+				(_, ret) =>
 				{
 					const game_id: string = ret.props.children.props.overview.m_gameid;
 					// console.log(game_id);
@@ -23,11 +25,37 @@ export const patchAppPage = (serverAPI: ServerAPI) => {
 							}
 						}
 					});
-					return {ret, vars};
-				})
+					return ret;
+				}
+		)
+		return props;
+	});
+}
+
+export const patchAppPageExperimental = (serverAPI: ServerAPI) => {
+	return serverAPI.routerHook.addPatch("/library/app/:appid", (props: { path: string, children: ReactElement}) =>
+	{
+		Patching.patch(props.children, {})
+			.afterPatchBasic(ret => ret.props, "renderFunc", (_, ret, vars) =>
+			{
+				const game_id: string = ret.props.children.props.overview.m_gameid;
+				// console.log(game_id);
+				serverAPI.callPluginMethod<{}, PlayTimes>("get_playtimes", {}).then((response: ServerResponse<PlayTimes>) =>
+				{
+					if (response.success)
+					{
+						// console.log(response.result)
+						if (response.result[game_id])
+						{
+							ret.props.children.props.details.nPlaytimeForever = +(response.result[game_id] / 60.0).toFixed(1);
+							console.log(+(response.result[game_id] / 60.0).toFixed(1));
+						}
+					}
+				});
+				return {ret, vars};
+			})
 			.done()
 		.done();
-		return props;
 	});
 }
 
